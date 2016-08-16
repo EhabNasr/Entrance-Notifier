2e7587731731fc54bc98e5880e73f1d74536cdd3
ehab <ehab.mohamed.nasr@gmail.com>
2016-08-16 16:05:31 +0200

    Version1 Initial setup.

--- Main.py
+++ Main.py
@@ -4,8 +4,11 @@ import Detector_pingBased as Detector
 def send_message():
     print('someone has arrived')
 
+
+counter = 0
 while True:
     if Detector.detect_person():
         while Detector.detect_person():
             pass
+        counter += 1
         send_message()
