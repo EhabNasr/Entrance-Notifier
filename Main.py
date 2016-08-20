3d8b7c9f4e210c117a86087fd369e5c7b4825e5c
ehab <ehab.mohamed.nasr@gmail.com>
2016-08-21 01:56:31 +0200

    Added sound

--- Main.py
+++ Main.py
@@ -1,14 +1,23 @@
 import Detector_pingBased as Detector
+from pygame import mixer
+import time
 
 
 def send_message():
     print('someone has arrived')
 
 
+mixer.init()
+alert = mixer.Sound('bell.wav')
 counter = 0
 while True:
+#    print(Detector.detect_person())
     if Detector.detect_person():
-        while Detector.detect_person():
-            pass
+        alert.play()
+        time.sleep(0.5)
+#        while Detector.detect_person():
+#            pass
         counter += 1
         send_message()
+        print(counter)
+
